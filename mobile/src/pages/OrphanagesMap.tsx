1017e16829dac580b5679b43d636ccc68e10c9ea
victor47n <victor47n@gmail.com>
2020-10-19 22:23:32 -0300

    Inicio mobile

---
+++ mobile/src/pages/OrphanagesMap.tsx
@@ -0,0 +1,116 @@
+import React from 'react';
+import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
+import MapView, { Marker, Callout, PROVIDER_GOOGLE } from 'react-native-maps';
+import { Feather } from '@expo/vector-icons';
+
+import mapMarker from '../images/map-marker.png';
+import { useNavigation } from '@react-navigation/native';
+
+export default function OrphanagesMap() {
+    const navigation = useNavigation();
+
+    function handleNavigateToOrphanageDetails() {
+        navigation.navigate('OrphanageDetails');
+    }
+
+    return (
+        <View style={styles.container}>
+            <MapView style={styles.map}
+                provider={PROVIDER_GOOGLE}
+                initialRegion={{
+                    latitude: -21.8379979,
+                    longitude: -46.8982355,
+                    latitudeDelta: 0.008,
+                    longitudeDelta: 0.008
+                }}
+            >
+                <Marker
+                    icon={mapMarker}
+                    calloutAnchor={{
+                        x: 2.7,
+                        y: 0.8,
+                    }}
+                    coordinate={{
+                        latitude: -21.8379979,
+                        longitude: -46.8982355,
+                    }}
+                >
+                    <Callout tooltip onPress={handleNavigateToOrphanageDetails}>
+                        <View style={styles.calloutContainer}>
+                            <Text style={styles.calloutText}>
+                                Lar das meninas
+                            </Text>
+                        </View>
+                    </Callout>
+                </Marker>
+            </MapView>
+
+            <View style={styles.footer}>
+                <Text style={styles.footerText}>2 orfanatos encontrados</Text>
+
+                <TouchableOpacity style={styles.createOrphanageButton} onPress={() => { }}>
+                    <Feather name="plus" size={20} color="#FFFFFF" />
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+
+    map: {
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height,
+    },
+
+    calloutContainer: {
+        width: 146,
+        height: 46,
+        paddingHorizontal: 16,
+        backgroundColor: 'rgba(255, 255, 255, 0.8)',
+        borderRadius: 16,
+        justifyContent: 'center',
+    },
+
+    calloutText: {
+        fontFamily: 'Nunito_700Bold',
+        fontSize: 14,
+        color: '#0089A5',
+    },
+
+    footer: {
+        position: 'absolute',
+        left: 24,
+        right: 24,
+        bottom: 32,
+
+        backgroundColor: '#FFF',
+        borderRadius: 20,
+        height: 56,
+        paddingLeft: 24,
+
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+
+        elevation: 3,
+    },
+
+    footerText: {
+        fontFamily: 'Nunito_700Bold',
+        color: '#8FA7B3',
+    },
+
+    createOrphanageButton: {
+        width: 56,
+        height: 56,
+        backgroundColor: '#15C3D6',
+        borderRadius: 20,
+
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+});
